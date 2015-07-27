52347a6670cf0210f4529950f235101cac382d33
Juan Pablo Bordes <juan.bordes@mercadolibre.com>
2015-07-27 03:06:27 -0300

    Agrego el param 'optimized' para pasarle al getMostRepeatedChars.sh

--- jpb/Problema1.groovy
+++ jpb/Problema1.groovy
@@ -1,14 +1,25 @@
 package jpb
 
+import groovy.time.*
+
 public class Problema1{
 
 	public static void main(String[] args){
 		def scanner = new TextScanner()
+		def opt = (args[1] == "t" || args[1] == "true")
 		
 		if(args && !args[0].isEmpty()){
-			scanner.readInput(args[0])		
-			def result = scanner.getMostRepeatedChars()
-			
+			scanner.readInput(args[0])
+			def result
+
+			if(opt){
+				println "OPT"
+				result = scanner.getMostRepeatedChars(args[1])
+			} else {
+				println "No OPT"
+				result = scanner.getMostRepeatedChars()
+			}
+
 			if(result){
 				if(result.size() > 1)
 					println "Los caracteres mas repetidos son: " + result
